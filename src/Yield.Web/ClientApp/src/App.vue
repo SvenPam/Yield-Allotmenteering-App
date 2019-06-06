84638024014bab4fee67fd82fe093eea219cf750
Jax Steele <jacquie.steele@outlook.com>
2019-06-06 22:00:04 +0100

    Started frontend work

--- src/Yield.Web/ClientApp/src/App.vue
+++ src/Yield.Web/ClientApp/src/App.vue
@@ -1,12 +1,7 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Yield</span>
-        <span class="font-weight-light">Allotmenteering</span>
-      </v-toolbar-title>
-    </v-toolbar>
 
+<header-bar></header-bar>
     <v-content>
       <v-container fluid>
         <router-view></router-view>
@@ -16,9 +11,16 @@
 </template>
 
 <script>
-export default {
-  name: "App",
-  components: {},
+  import { Component, Vue } from "vue-property-decorator";
+import Header from "./components/Header.vue";
+  
+  @Component ({
+    components: {
+        'header-bar': Header
+
+  }
+  })
+export default class App extends Vue  {
   data() {
     return {};
   }
--- src/Yield.Web/ClientApp/src/views/Home.vue
+++ src/Yield.Web/ClientApp/src/views/Home.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="home">
-
+    
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
-@Component({
-  components: {
-  }
-})
+
+@Component
+
 export default class Home extends Vue {}
 </script>
